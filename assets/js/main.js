98ae6b65cd873da4f35ad52df69cbf2766b3da5a
zrnteacher <70248260+zrnteacher@users.noreply.github.com>
2021-02-04 16:29:40 +0800

    hello

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,13 +99,6 @@
 
 	// Header (narrower + mobile).
 
-		// Toggle.
-			$(
-				'<div id="headerToggle">' +
-					'<a href="#header" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
 
 		// Header.
 			$('#header')
@@ -162,3 +155,40 @@ function afterLogin(profile){
 function settleAccount(){
     var payment= window.open("/settleAccount.html", "Let'sPay", "");
 }
+
+
+//Shopping Card
+
+
+
+$(document).ready(function() {
+	$('.buy').click(function() {
+	  $('.card p').slideUp();
+	  $('.menu').css('top', '40%');
+	});
+	$('.cancel').click(function() {
+	  $('.card p').slideDown();
+	  $('.menu').css('top', '80%');
+	});
+	$('.addcart').click(function() {
+	  var y = 180;
+	  $('.content').css({
+		'transform': 'rotateY(' + y + 'deg)'
+	  });
+  
+	  $('.content').delay(3000).queue(function() {
+		$(this).css({
+		  'transform': 'rotateY(0deg)'
+		}).dequeue();
+		$('.card p').slideDown();
+		$('.menu').css('top', '80%');
+		$('.content .icon').toggle();
+	  });
+	});
+  });
+  
+  function change(){
+	  store1[0] = document.getElementById("num").value;
+  }
+  
+  
